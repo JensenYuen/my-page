321020899ce0e8717502207163f3f5d5bfc4fae7
jensen yuen <jensenyuen@outlook.com>
2022-12-10 03:52:07 +0900

    NavBar style change

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { NavbarBrand } from 'react-bootstrap';
 import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
@@ -9,17 +10,24 @@ interface Props {
 const NavBar = ({ title, links }: Props) => {
 
   return (
-    <Nav className="justify-content-center border-bottom border-dark border-opacity-75 p-2 fixed-top bg-light" activeKey="/home">
-      {
-        links.map((link: string) => {
-          return (
-            <Nav.Item>
-              <Nav.Link href={`/#/${link}`}>{link}</Nav.Link>
-              {/* <Link to={`/${link}`}>{link}</Link> */}
-            </Nav.Item>
-          )
-        })
-      }
+    <Nav className="justify-content-between border-bottom
+      border-dark border-opacity-75 fixed-top bg-light px-5 p-2"
+      activeKey="/home"
+    >
+      <div className='d-flex'>
+        <NavbarBrand><p className='m-0 fs-4'>Jensen.Y</p></NavbarBrand>
+      </div>
+      <div className='d-flex flex-row'>
+        {
+          links.map((link: string) => {
+            return (
+              <Nav.Item>
+                <Nav.Link href={`/#/${link}`}>{link}</Nav.Link>
+              </Nav.Item>
+            )
+          })
+        }
+      </div>
     </Nav>
   );
 }
