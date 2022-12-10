88ef875e2da3239f6281f40d1e36ab1b373b0615
jensen yuen <jensenyuen@outlook.com>
2022-12-11 06:33:58 +0900

    changes to components

--- src/components/CustomCard.tsx
+++ src/components/CustomCard.tsx
@@ -1,26 +1,50 @@
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import react from 'react'
 import Card from 'react-bootstrap/Card';
+import { useTranslation } from 'react-i18next';
 
 interface props {
   className?: string;
   title: string;
-  text: string;
-  info: string;
-  link: string;
+  img: string;
+  link?: string;
 }
 
-const CustomCard = ({ className, title, text, info, link }:props) => {
+const CustomCard = ({ className, title, img, link }:props) => {
+  const { t } = useTranslation();
+
+  const titles:any = {
+    "offgrid": `${t("portfoilo.offgrid.title")}`,
+    "scubadoo": `${t("portfoilo.scubadoo.title") }`,
+    "gathersg": `${t("portfoilo.gathersg.title") }`
+  }
+  const descriptions:any = {
+    "offgrid": `${t("portfoilo.offgrid.description")}`,
+    "scubadoo": `${t("portfoilo.scubadoo.description")}`,
+    "gathersg": `${t("portfoilo.gathersg.description")}`
+  }
+  const infos:any = {
+    "offgrid": `${t("portfoilo.offgrid.info")}`,
+    "scubadoo": `${t("portfoilo.scubadoo.info")}`,
+    "gathersg": `${t("portfoilo.gathersg.info")}`
+  }
 
   return (
     <div className={`${className}`}>
       <Card className='bg-dark h-100 text-white'>
-        <Card.Img src={link} variant="top" alt="Card image" className='h-100'/>
+        <Card.Img src={img} variant="top" alt="Card image" className='h-100'/>
+        <Card.ImgOverlay className='d-flex justify-content-end align-items-end'>
+          { link &&
+            <Card.Link href={link}><FontAwesomeIcon color='white' size='1x' icon={faGithub} /></Card.Link>
+          }
+        </Card.ImgOverlay>
         <Card.Body>
           <div className='d-flex'>
-            <Card.Title className='px-1'>{title}</Card.Title>
-            <Card.Text>{text}</Card.Text>
+            <Card.Title className='px-1'>{titles[title]}</Card.Title>
+            <Card.Text>{descriptions[title]}</Card.Text>
           </div>
-          <Card.Text>{info}</Card.Text>
+          <Card.Text className='text-wrap'>{infos[title]}</Card.Text>
         </Card.Body>
       </Card>
     </div>
--- src/pages/Portfoilo.tsx
+++ src/pages/Portfoilo.tsx
@@ -4,27 +4,22 @@ import CustomCard from '../components/CustomCard';
 import offgrid from '../assets/offgrid.png'
 import scubadoo from '../assets/scubadoo.png'
 import gathersg from '../assets/gathersg.png'
+import { useTranslation } from 'react-i18next';
 
 interface CardInfo {
   title: string;
-  text: string;
-  info: string;
-  link: string;
+  img: string;
+  link?: string;
 }
 
 const Portfoilo = () => {
+  const { t } = useTranslation();
+
   const infos: CardInfo[] = [];
-  const infoText ={
-    offgrid: `Offgrid was developed to help novice hikers prepare for their hiking trip.
-      We identified the lack of concise information for hikers and the lack of communication between hikers and their family/friends.`,
-    scubadoo: `Scubadoo is a marketplace clone (of Padi) developed within 3 days,
-      this app was created to help divers locate dive centers and dive listings around the world.`,
-    gathersg: `Maintained and developed features. Developed REST API for system integrations.
-    Engaged with stakeholders and users to provide support for their issues. `
-}
-  const productOffgid = { title: "OffGrid", text: "Final project", link: offgrid, info: infoText.offgrid};
-  const productScubadoo = { title: "Scubadoo", text: "Marketplace clone", link: scubadoo, info: infoText.scubadoo };
-  const productGathersg = { title: "GatherSG", text: "Case management", link: gathersg, info: infoText.gathersg };
+
+  const productOffgid = { title: "offgrid", img: offgrid, link: 'https://github.com/eztree/offgrid' };
+  const productScubadoo = { title: "scubadoo", img: scubadoo, link: 'https://github.com/suansen/scubadoo' };
+  const productGathersg = { title: "gathersg", img: gathersg };
   infos.push(productOffgid, productScubadoo, productGathersg);
 
   const renderCards = () => {
@@ -34,8 +29,7 @@ const Portfoilo = () => {
           key={info.title}
           className='col'
           title={info.title}
-          text={info.text}
-          info={info.info}
+          img={info.img}
           link={info.link}
         />
       )
@@ -48,8 +42,8 @@ const Portfoilo = () => {
     <main>
       <Container fluid style={{ margin: "auto", paddingTop: "2vw", paddingInline: "5vw" }}>
         <div className='py-5 px-4 pb-0'>
-          <h2 className='pb-2 border-bottom'>Portfoilo</h2>
-          <p className='fs-5'>This section contains work that I was involved in or developed.</p>
+          <h2 className='pb-2 border-bottom'>{t("portfoilo.header")}</h2>
+          <p className='fs-5'>{t("portfoilo.subtext")}</p>
         </div>
         <div className="row row-cols-1 row-cols-lg-3 g-3 py-2 m-0"
           style={{ paddingInline: "5vw" }}>
