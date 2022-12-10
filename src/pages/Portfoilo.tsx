ceca606dc5d60cf7bd266018c02f3cea7c40f32e
jensen yuen <jensenyuen@outlook.com>
2022-12-10 20:02:34 +0900

    Changes to porfoilo page

--- src/pages/Portfoilo.tsx
+++ src/pages/Portfoilo.tsx
@@ -1,11 +1,52 @@
 import react from 'react'
-import { Container } from 'react-bootstrap';
+import { Container, ImageProps } from 'react-bootstrap';
+import CustomCard from '../components/CustomCard';
+import offgrid from '../assets/offgrid.png'
+import scubadoo from '../assets/scubadoo.png'
+import gathersg from '../assets/gathersg.png'
+import { Url } from 'url';
+
+interface CardInfo {
+  title: string;
+  text: string;
+  link: string;
+}
 
 const Portfoilo = () => {
+  const infos: CardInfo[] = [];
+  const productOne = { title: "OffGrid", text: "Final project", link: offgrid};
+  const productTwo = { title: "Scubadoo", text: "EC clone", link: scubadoo };
+  const productThree = { title: "GatherSG", text: "Case management", link: gathersg };
+  infos.push(productOne, productTwo, productThree);
+
+  const renderCards = () => {
+    const cards = infos.map((info) => {
+      return(
+        <CustomCard
+          className='col'
+          title={info.title}
+          text={info.text}
+          link={info.link}
+        />
+      )
+    })
+
+    return cards
+  }
+
   return (
-    <Container fluid>
-      content to be added
-    </Container>
+    <main>
+      <Container fluid style={{ margin: "auto", paddingTop: "2vw", paddingInline: "5vw" }}>
+        <div className='py-5 px-4 pb-0'>
+          <h2 className='pb-2 border-bottom'>Portfoilo</h2>
+          <p>This section contains work that I was involved in or developed</p>
+        </div>
+        <div className="row row-cols-1 row-cols-lg-3 g-3 py-2 m-0"
+          style={{ paddingInline: "5vw" }}>
+          {renderCards()}
+        </div>
+      </Container>
+    </main>
   );
 }
 
