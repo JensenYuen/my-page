a4f4e07b4f39e5cadb090caf66b07328346f9262
jensen yuen <jensenyuen@outlook.com>
2022-12-11 20:04:39 +0900

    Added new contants

--- src/constants/constant.jsx
+++ src/constants/constant.jsx
@@ -2,6 +2,10 @@ import offgrid from '../assets/images/offgrid.png'
 import scubadoo from '../assets/images/scubadoo.png'
 import gathersg from '../assets/images/gathersg.png'
 import profile from '../assets/images/profile.png'
+import bootstrap from '../assets/images/logos/bootstrap.png'
+import github from '../assets/images/logos/github.png'
+import react from '../assets/images/logos/react.png'
+import typescript from '../assets/images/logos/typescript.png'
 
 export const ProfileImg = profile;
 
@@ -14,4 +18,11 @@ export const ProjImgs = {
 export const ProjLinks = {
   offgird: 'https://github.com/eztree/offgrid',
   scubadoo: 'https://github.com/suansen/scubadoo'
+}
+
+export const Logos = {
+  bootstrap: bootstrap,
+  github: github,
+  react: react,
+  typescript: typescript
 }
